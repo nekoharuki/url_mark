750dc84d590b7125633cd40710f0b5dc7ae50c63
nekoharuki <harukiyamada1212@gmail.com>
2025-02-16 17:01:29 +0900

    コントローラー設計

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "likes/index"
+  get "urls/index"
+  get "urls/new"
+  get "urls/edit"
   get "/" => "home#top"
   get "users/index" => "users#index"
 end
---
+++ test/controllers/urls_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class UrlsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get urls_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get urls_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get urls_edit_url
+    assert_response :success
+  end
+end
