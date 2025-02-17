622aa2a06c857f3e35292f2a0f9d0c72064509c3
nekoharuki <harukiyamada1212@gmail.com>
2025-02-17 15:52:19 +0900

    urlの機能

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,10 +1,38 @@
 class UrlsController < ApplicationController
   def index
+    @urls=Url.all
   end
 
   def new
+    @url=Url.new
   end
 
   def edit
+    @url=Url.find_by(id: params[:id])
+  end
+
+  def update
+    @url=Url.find_by(id: params[:id])
+    @url.name=params[:name]
+    @url.url=params[:url]
+    @url.save
+    redirect_to("/urls/#{@url.id}")
+  end
+  def show
+  @url=Url.find_by(id: params[:id])
+  end
+  def create
+    @url=Url.new(url: params[:url], name: params[:name], user_id: 1)
+    if @url.save
+      redirect_to("/urls/index")
+    else
+    end
+  end
+
+
+  def destroy
+    @url=Url.find_by(id: params[:id])
+    @url.destroy
+    redirect_to("/urls/index")
   end
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,6 @@
 class Url < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :url, presence: true
 end
