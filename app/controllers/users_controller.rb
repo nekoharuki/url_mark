7fbabafb4308b32d7984f3e10aef1410cfeb40b1
nekoharuki <harukiyamada1212@gmail.com>
2025-02-17 17:15:34 +0900

    ユーザー編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,35 @@
 class UsersController < ApplicationController
   def index
+    @users=User.all
+  end
+  def show
+    @user=User.find_by(id: params[:id])
+  end
+  def new
+  @user=User.new
+  end
+  def create
+    @user=User.new(name: params[:name], email: params[:email], password: params[:password])
+    if @user.save
+    redirect_to("/users/index")
+    else
+      render_to_("/")
+    end
+  end
+  def edit
+    @user=User.find_by(id: params[:id])
+  end
+  def update
+    @user=User.find_by(id: params[:id])
+    @user.name=params[:name]
+    @user.email=params[:email]
+    @user.password=params[:password]
+    @user.save
+    redirect_to("/users/#{@user.id}")
+  end
+  def destroy
+    @user=User.find_by(id: params[:id])
+    @user.destroy
+    redirect_to("/users/index")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
+  get "users/index" => "users#index"
+  get "users/new" => "users#new"
+  post "users/create" => "users#create"
+  get "users/:id/edit" => "users#edit"
+  post "users/:id/update" => "users#update"
+  post "users/:id/destroy" => "users#destroy"
+  get "users/:id" => "users#show"
+
   post "urls/create" => "urls#create"
-  get "likes/index"
   get "urls/index" => "urls#index"
   get "urls/new" => "urls#new"
   post "urls/:id/destroy" => "urls#destroy"
   post "urls/:id/update" => "urls#update"
   get "urls/:id/edit" => "urls#edit"
   get "urls/:id" => "urls#show"
+
   get "/" => "home#top"
   get "/about" => "home#about"
   get "users/index" => "users#index"
