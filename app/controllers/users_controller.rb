651be92b76be64044a7117aaf24add3b2032708d
nekoharuki <harukiyamada1212@gmail.com>
2025-02-17 17:54:44 +0900

    userのインデントをそろえて

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,35 +1,56 @@
 class UsersController < ApplicationController
   def index
-    @users=User.all
+    @users = User.all
   end
+
   def show
-    @user=User.find_by(id: params[:id])
+    @user = User.find_by(id: params[:id])
   end
+
   def new
-  @user=User.new
+    @user = User.new
   end
+
   def create
-    @user=User.new(name: params[:name], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
     if @user.save
-    redirect_to("/users/index")
+      flash[:notice] = "ユーザー登録できました"
+      redirect_to("/users/index")
     else
-      render_to_("/")
+      flash[:alert] = "ユーザー登録できませんでした"
+      render("users/new")
     end
   end
+
   def edit
-    @user=User.find_by(id: params[:id])
+    @user = User.find_by(id: params[:id])
   end
+
   def update
-    @user=User.find_by(id: params[:id])
-    @user.name=params[:name]
-    @user.email=params[:email]
-    @user.password=params[:password]
-    @user.save
-    redirect_to("/users/#{@user.id}")
+    @user = User.find_by(id: params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "ユーザー編集できました"
+      redirect_to("/users/#{@user.id}")
+    else
+      flash[:alert] = "ユーザー編集できませんでした"
+      render("users/edit")
+    end
   end
+
   def destroy
-    @user=User.find_by(id: params[:id])
-    @user.destroy
-    redirect_to("/users/index")
+    @user = User.find_by(id: params[:id])
+    if @user.destroy
+      flash[:notice] = "ユーザー削除できました"
+      redirect_to("/users/index")
+    else
+      flash[:alert] = "ユーザー削除できませんでした"
+      redirect_to("/users/#{@user.id}")
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
