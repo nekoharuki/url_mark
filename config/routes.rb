c7715923f3fd6788da6ad53cf254615f55742260
nekoharuki <harukiyamada1212@gmail.com>
2025-02-17 13:44:54 +0900

    url_new

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  post "urls/create" => "urls#create"
   get "likes/index"
   get "urls/index"
-  get "urls/new"
+  get "urls/new" => "urls#new"
   get "urls/edit"
   get "/" => "home#top"
   get "/about" => "home#about"
