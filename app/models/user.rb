75cf1a6dafe8627aacf052e88dcf4331c9db6d98
nekoharuki <harukiyamada1212@gmail.com>
2025-02-27 12:03:04 +0900

    login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,20 @@ class User < ApplicationRecord
   has_secure_password
   validates :name, presence: true
   validates :email, presence: true
+  def self.from_omniauth(auth)
+    user = find_or_initialize_by(provider: auth.provider, uid: auth.uid)
+    if user.new_record?
+      existing_user = find_by(email: auth.info.email)
+      if existing_user
+        user = existing_user
+        user.update(provider: auth.provider, uid: auth.uid)
+      else
+        user.email = auth.info.email
+        user.name = auth.info.name
+        user.password = SecureRandom.hex(10)
+        user.save
+      end
+    end
+    user
+  end
 end
