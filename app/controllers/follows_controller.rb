5233da9bdc9ac277043799b1c1a6bdea5bf91c4c
nekoharuki <harukiyamada1212@gmail.com>
2025-02-28 21:57:21 +0900

    follow機能作成

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -3,19 +3,19 @@ class FollowsController < ApplicationController
   before_action :now_logout_check
   before_action :follow_check
 
-  def create
-    @follow=Follow.new(follower_user: @current_user.id, followed_user: params[:followed_user_id])
-    if @follow.save
-      flash[:notice]="フォロー登録できました"
-      redirect_to("/urls/index")
-    else
-      flash[:alert]="フォロー登録に失敗しました"
-      redirect_to("/urls/index")
-    end
+def create
+  @follow=Follow.new(follower_user: @current_user.id, followed_user: params[:followed_user])
+  if @follow.save
+    flash[:notice]="フォロー登録できました"
+    redirect_to("/urls/index")
+  else
+    flash[:alert]="フォロー登録に失敗しました"
+    redirect_to("/urls/index")
   end
+end
 
   def destroy
-    @follow=Follow.find_by(follower_user: @current_user.id, followed_user: params[:followed_user_id])
+    @follow=Follow.find_by(follower_user: @current_user.id, followed_user: params[:followed_user])
     if @follow
       if @follow.destroy
         flash[:notice]="フォロー削除できました"
@@ -31,9 +31,9 @@ class FollowsController < ApplicationController
   end
 
   def follow_check
-    @user=User.find_by(id: params[:followed_user_id])
-    if @user==nil
-      flash[:alert]="フォロー登録に失敗しました"
+    @user=User.find_by(id: params[:followed_user])
+    if @user==nil || @user.id==@current_user.id
+      flash[:alert]="失敗しました---"
       redirect_to("/urls/index")
     end
   end
--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,6 +1,4 @@
 class Follow < ApplicationRecord
-  belongs_to :user
-
   validates :follower_user, presence: true
   validates :followed_user, presence: true
 end
